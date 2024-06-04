ad6c7030728f791ca4f924bdadbee4a2eafeb8f5
jveselkov <jveselkov@bk.ru>
2024-06-04 15:07:13 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Space } from "antd";
+
+function App() {
+  return (
+    <>
+      <Space align="center">
+        <div style={{ minHeight: "20rem" }}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
+          repellendus ad voluptatum quidem dolore blanditiis praesentium id
+          eaque? Excepturi, fugit!
+        </div>
+      </Space>
+    </>
+  );
+}
+
+export default App;
